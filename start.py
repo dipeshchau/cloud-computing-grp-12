b2f71d3b9ecfeef8172e1ac30ec396d854e3b30c
Dipeshchy <dipeshchaudhary5@gmail.com>
2022-09-28 17:53:44 +0200

    download mongodb changes

--- start.py
+++ start.py
@@ -205,21 +205,21 @@ userDataDB = ('#!/bin/bash\n'
               '# extra repo for RedHat rpms\n'
               'yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm\n'
               '# essential tools\n'
-                'yum install -y joe htop git\n'
-                     '# mongodb\n'
-                     'yum install -y nodejs mongodb\n'
-                     '\n'
-                     'wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -',
-                     '\n'
-                     'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list',
-                     '\n'
-                     'sudo apt-get update'
-                     '\n'
-                     'sudo apt-get install -y mongodb-org',
-                     '\n'
-                     'systemctl start mongod',
+              'yum install -y joe htop git\n'
+              '# mongodb\n'
+              'yup -y install mongodb'
               '\n'
-              '
+              'yum install -y nodejs mongodb\n'
+              'wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -'
+              '\n'
+              'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list'
+              '\n'
+              'sudo apt-get update'
+              '\n'
+              'sudo apt-get install -y mongodb-org'
+              '\n'
+              'systemctl start mongod'
+              '\n'
               )
 # convert user-data from script with: cat install-mysql | sed "s/^/'/; s/$/\\\n'/"
 
